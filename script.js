8bdabd3b4fbdec76d2c2172c78ac2d0f2a32e406
Brianclikclak <brianclikclak@hotmail.com>
2023-06-25 22:51:18 +0200

    function for cards

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const poke_container = document.getElementById('poke-container');
 const pokemon_count = 150;
 const colors = {
-  fire: '#FDDFDF',
-  grass: '#DEFDE0',
-  electric: '#FCF7DE',
-  water: '#DEF3FD',
-  ground: '#f4e7da',
-  rock: '#d5d5d4',
-  fairy: '#fceaff',
-  poison: '#98d7a5',
-  bug: '#f8d5a3',
-  dragon: '#97b3e6',
-  psychic: '#eaeda1',
-  flying: '#F5F5F5',
-  fighting: '#E6E0D4',
-  normal: '#F5F5F5'
+  fire: '#FD4217',
+  grass: '#228F41',
+  electric: '#eed535',
+  water: '#2F72FF',
+  ground: '#ab9842',
+  rock: '#a38c21',
+  fairy: '#fdb9e9',
+  poison: '#8F49FF',
+  bug: '#729F3F',
+  dragon: '#53a4cf',
+  psychic: '#f366b9',
+  flying: '#3dc7ef',
+  fighting: '#d56723',
+  normal: '#A4ACAF',
+  ghost : '#7b62a3 ',
+  ice : '#51c4e7'
+
 };
 
 const getPokemons = async () => {
@@ -33,6 +35,9 @@ const getPokemons = async () => {
           .then((response) => {
             const pokemonData = response.data;
             console.log(pokemonData);
+            createCard(pokemonData);
+            
+            
           });
       }
     }
@@ -42,3 +47,43 @@ const getPokemons = async () => {
 };
 
 getPokemons();
+
+function createCard(pokemonData){
+  const poke_container = document.getElementById('poke-container');
+
+  const pokeTypes = pokemonData.types.map(type => type.type.name);
+  const type = pokeTypes[0];
+  const color = colors[type];
+
+  let containerPokemon = document.createElement("div")
+  containerPokemon.classList.add('pokemon');
+  poke_container.appendChild(containerPokemon);
+
+  let pokeImg = document.createElement("img");
+  pokeImg.src = pokemonData.sprites.front_default;
+  containerPokemon.appendChild(pokeImg);
+
+  let info = document.createElement("div");
+  info.classList.add('info')
+  containerPokemon.appendChild(info);
+
+  let order = document.createElement("p");
+  order.classList.add ('number');
+  order.textContent = `# ${pokemonData.id.toString().padStart(3, '0')}`;
+  info.appendChild(order)
+
+  let pokeName = document.createElement("p");
+  pokeName.classList.add('name');
+  pokeName.textContent = `${pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1).toLowerCase()}`;
+  info.appendChild(pokeName);
+
+  let pokeType = document.createElement("p");
+  pokeType.classList.add('type');
+  pokeType.textContent = `${type} `;
+  pokeType.style.backgroundColor = color;
+  info.appendChild(pokeType);
+
+
+}
+
+
